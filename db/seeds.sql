0f5cf750744d855da34fdbf3fa9937d3de10f8ed
Jason Perez <jasandper@icloud.com>
2020-10-19 12:14:19 -0500

    setup database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Bacon', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Jalapeno', true);
+INSERT INTO burgers (burger_name,devoured) VALUES ('BBQ', true);
